57975c9feeb574d9e95ac0e3c7337ba9dc36a212
maarten <maarten@fd7d9fed-2008-0410-9570-f3208bcf4cb9>
2014-12-30 15:25:57 +0000

    O.a. XSLWEB-33 Log extension function kan nu willekeurige sequences loggen

--- src/main/java/nl/armatiek/xslweb/saxon/functions/serialize/Serialize.java
+++ src/main/java/nl/armatiek/xslweb/saxon/functions/serialize/Serialize.java
@@ -3,25 +3,16 @@ package nl.armatiek.xslweb.saxon.functions.serialize;
 import java.io.StringWriter;
 import java.util.Properties;
 
-import javax.xml.transform.stream.StreamResult;
-
-import net.sf.saxon.Configuration;
-import net.sf.saxon.event.Receiver;
 import net.sf.saxon.expr.XPathContext;
-import net.sf.saxon.lib.ExtensionFunctionCall;
 import net.sf.saxon.lib.ExtensionFunctionDefinition;
-import net.sf.saxon.om.AxisInfo;
-import net.sf.saxon.om.Item;
 import net.sf.saxon.om.NodeInfo;
 import net.sf.saxon.om.Sequence;
-import net.sf.saxon.om.SequenceIterator;
 import net.sf.saxon.om.StructuredQName;
-import net.sf.saxon.pattern.NodeKindTest;
 import net.sf.saxon.trans.XPathException;
-import net.sf.saxon.tree.iter.AxisIterator;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
 import nl.armatiek.xslweb.configuration.Definitions;
+import nl.armatiek.xslweb.saxon.functions.ExtensionFunctionCall;
 
 /**
  * 
@@ -61,31 +52,12 @@ public class Serialize extends ExtensionFunctionDefinition {
     
     @Override
     public StringValue call(XPathContext context, Sequence[] arguments) throws XPathException {                            
-      Properties props = new Properties();
+      Properties props = null;
       if (arguments.length == 2) {
-        NodeInfo paramsDoc = (NodeInfo) arguments[1].head();
-        NodeInfo paramsElem = paramsDoc.iterateAxis(AxisInfo.CHILD, NodeKindTest.ELEMENT).next(); 
-        AxisIterator iter = paramsElem.iterateAxis(AxisInfo.CHILD, NodeKindTest.ELEMENT);
-        NodeInfo paramElem;
-        while ((paramElem = iter.next()) != null) {
-          props.put(paramElem.getLocalPart(), paramElem.getAttributeValue("", "value"));
-        }  
+        props = getOutputProperties((NodeInfo) arguments[1].head());
       }
-      Configuration config = context.getConfiguration();
       StringWriter sw = new StringWriter();
-      Item item;
-      Sequence seq = arguments[0];
-      SequenceIterator iter = seq.iterate();
-      while ((item = iter.next()) != null) {
-        if (item instanceof NodeInfo) {
-          NodeInfo nodeInfo = (NodeInfo) item;
-          Receiver serializer = config.getSerializerFactory().getReceiver(new StreamResult(sw), 
-              config.makePipelineConfiguration(), props);
-          nodeInfo.copy(serializer, NodeInfo.ALL_NAMESPACES, 0);
-        } else {                             
-          sw.append(item.getStringValue());
-        }
-      }    
+      serialize(arguments[1], sw, props);
       return StringValue.makeStringValue(sw.toString());              
     }
   }
