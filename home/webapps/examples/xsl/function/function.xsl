f47631d292e00083282e9156e05048294cfa9aa2
Maarten Kroon <maarten.kroon@koop.overheid.nl>
2020-05-15 16:57:26 +0200

    Checkpoint of development of dynamic/scripted XPath extension functions

--- home/webapps/examples/xsl/function/function.xsl
+++ home/webapps/examples/xsl/function/function.xsl
@@ -6,12 +6,140 @@
   xmlns:xhtml="http://www.w3.org/1999/xhtml"
   xmlns:function="http://www.armatiek.com/xslweb/functions/function"
   xmlns:output="http://www.w3.org/2010/xslt-xquery-serialization"
+  xmlns:err="http://www.w3.org/2005/xqt-errors"
   exclude-result-prefixes="#all"
   version="3.0">
   
   <xsl:import href="../common/example-page.xsl"/>
   
   <xsl:template name="title" as="xs:string">Example 21: Dynamic functions extensions</xsl:template>
+  
+  <xsl:variable name="function-ns-uri" as="xs:string">http://www.armatiek.com/xslweb/functions/tests</xsl:variable>
+  
+  <xsl:template name="register-function">
+    <xsl:param name="function-name" as="xs:QName"/>
+    <xsl:param name="code-units" as="xs:string*"/>
+    <xsl:variable name="result" select="function:register($function-name, $code-units)" as="element(function:diagnostics)?"/>
+    <xsl:if test="$result/function:diagnostic/@kind = 'ERROR'">
+      <p>Error registering function <xsl:value-of select="$function-name"/>:</p>
+      <pre class="prettyprint lang-xml linenums">
+        <xsl:sequence select="serialize($result, $output-parameters)"/>
+      </pre>
+    </xsl:if>
+  </xsl:template>
+  
+  <xsl:template name="test-1">
+    <xsl:try>
+      <p>Calculates the longest common subsequence of two simple Strings</p>
+      <xsl:variable name="function-name" select="QName($function-ns-uri, 'lcs')" as="xs:QName"/>
+      <xsl:variable name="code-units" as="xs:string+">
+        <xsl:text>
+        <![CDATA[
+        import org.apache.commons.text.similarity.LongestCommonSubsequence;
+    
+        public class LongestCommonSubsequenceFunction {
+    
+          public String call(String left, String right) {
+            LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+            return lcs.longestCommonSubsequence(left, right).toString();
+          }
+          
+        }
+        ]]>  
+        </xsl:text>
+      </xsl:variable>
+      <xsl:call-template name="register-function">
+        <xsl:with-param name="function-name" select="$function-name" as="xs:QName"/>
+        <xsl:with-param name="code-units" select="$code-units" as="xs:string+"/>
+      </xsl:call-template>
+      <p>
+        <xsl:sequence select="function:call($function-name, 'ABACCD', 'ACDF')"/>
+      </p>
+      <xsl:catch>
+        <p>
+          <xsl:value-of select="$err:description || ' (' || $err:code || ' line: ' || $err:line-number || ', column: ' || $err:column-number || ')'"/>  
+        </p>
+      </xsl:catch>
+    </xsl:try>
+  </xsl:template>
+  
+  <xsl:template name="test-2">
+    <xsl:try>
+      <p>Sorts the specified range of a float array into ascending order.</p>
+      <xsl:variable name="function-name" select="QName($function-ns-uri, 'sort-floats')" as="xs:QName"/>
+      <xsl:variable name="code-units" as="xs:string+">
+        <xsl:text>
+        <![CDATA[
+        public class SortFloatsFunction {
+  
+          public float[] call(float[] a, int fromIndex, int toIndex) {
+            java.util.Arrays.sort(a, fromIndex, toIndex);
+            return a;
+          }
+        
+        }
+        ]]>  
+        </xsl:text>
+      </xsl:variable>
+      <xsl:call-template name="register-function">
+        <xsl:with-param name="function-name" select="$function-name" as="xs:QName"/>
+        <xsl:with-param name="code-units" select="$code-units" as="xs:string+"/>
+      </xsl:call-template>
+      <p>
+        <xsl:sequence select="function:call($function-name, (1.2, 1.3, 1.5), 1, 2)"/>
+      </p>
+      <xsl:catch>
+        <p>
+          <xsl:value-of select="$err:description || ' (' || $err:code || ' line: ' || $err:line-number || ', column: ' || $err:column-number || ')'"/>  
+        </p>
+      </xsl:catch>
+    </xsl:try>
+  </xsl:template>
+  
+  <xsl:template name="test-3">
+    <xsl:try>
+      <p>Void return value, use of implicit object XPathContext</p>
+      <xsl:variable name="function-name" select="QName($function-ns-uri, 'void')" as="xs:QName"/>
+      <xsl:variable name="code-units" as="xs:string+">
+        <xsl:text>
+        <![CDATA[
+        import net.sf.saxon.expr.XPathContext;
+        
+        public class VoidFunction {
+  
+          public void call(XPathContext context, String text) {
+            System.out.println(text + ", your Saxon edition is: " + context.getConfiguration().getEditionCode());
+          }
+        
+        }
+        ]]>  
+        </xsl:text>
+      </xsl:variable>
+      <xsl:call-template name="register-function">
+        <xsl:with-param name="function-name" select="$function-name" as="xs:QName"/>
+        <xsl:with-param name="code-units" select="$code-units" as="xs:string+"/>
+      </xsl:call-template>
+      <p>
+        <xsl:sequence select="function:call($function-name, 'Hello World')"/>
+      </p>
+      <xsl:catch>
+        <p>
+          <xsl:value-of select="$err:description || ' (' || $err:code || ' line: ' || $err:line-number || ', column: ' || $err:column-number || ')'"/>  
+        </p>
+      </xsl:catch>
+    </xsl:try>
+  </xsl:template>
+  
+  <xsl:template name="tab-contents-1">
+    <xsl:call-template name="test-1"/>
+    <xsl:call-template name="test-2"/>
+    <xsl:call-template name="test-3"/>
+  </xsl:template>
+  
+  <!-- These variables can be ignored: -->
+  <xsl:variable name="pipeline-xsl" select="document('')" as="document-node()"/>
+  
+  <xsl:variable name="template-name" as="xs:string">function</xsl:variable>
   
   <xsl:variable name="output-parameters" as="node()">
     <output:serialization-parameters>
@@ -20,42 +148,5 @@
       <output:omit-xml-declaration value="yes"/>
     </output:serialization-parameters>  
   </xsl:variable>
-  
-  <xsl:template name="tab-contents-1">
-    <p>Text</p>
-    <xsl:variable name="function-name" select="QName('http://www.armatiek.com/xslweb/functions/text', 'lcs')"/>
-    <xsl:variable name="java-code" as="xs:string">
-      <![CDATA[
-      import org.apache.commons.text.similarity.LongestCommonSubsequence;
-      
-      public class LongestCommonSubsequenceFunction {
-      
-        public String call(String left, String right) {
-          LongestCommonSubsequence lcs = new LongestCommonSubsequence();
-          return lcs.longestCommonSubsequence(left, right).toString();
-        }
-      
-      }
-      ]]>
-    </xsl:variable>
-    <xsl:variable name="result" select="function:register($function-name, ($java-code))" as="element(function:diagnostics)?"/>
-    <xsl:choose>
-      <xsl:when test="$result/function:diagnostic/@kind = 'ERROR'">
-        <p>Error compiling/registering function:</p>
-        <pre class="prettyprint lang-xml linenums">
-          <xsl:sequence select="serialize($result, $output-parameters)"/>
-        </pre>
-      </xsl:when>
-      <xsl:otherwise>
-        <p>Result of function call:</p>
-        <xsl:sequence select="function:call($function-name, 'ABACCD', 'ACDF')"/>  
-      </xsl:otherwise>
-    </xsl:choose>
-  </xsl:template>
-  
-  <!-- These variables can be ignored: -->
-  <xsl:variable name="pipeline-xsl" select="document('')" as="document-node()"/>
-  
-  <xsl:variable name="template-name" as="xs:string">function</xsl:variable>
   
 </xsl:stylesheet>
--- src/main/java/nl/armatiek/xslweb/saxon/functions/function/DynamicExtensionFunctionDefinition.java
+++ src/main/java/nl/armatiek/xslweb/saxon/functions/function/DynamicExtensionFunctionDefinition.java
@@ -2,24 +2,77 @@ package nl.armatiek.xslweb.saxon.functions.function;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import net.sf.saxon.Configuration;
 import net.sf.saxon.expr.JPConverter;
 import net.sf.saxon.expr.PJConverter;
+import net.sf.saxon.expr.StaticProperty;
 import net.sf.saxon.expr.XPathContext;
+import net.sf.saxon.om.Function;
 import net.sf.saxon.om.Item;
+import net.sf.saxon.om.NodeInfo;
 import net.sf.saxon.om.Sequence;
 import net.sf.saxon.om.SequenceIterator;
-import net.sf.saxon.om.SequenceTool;
+import net.sf.saxon.om.StandardNames;
 import net.sf.saxon.om.StructuredQName;
+import net.sf.saxon.s9api.QName;
 import net.sf.saxon.trans.XPathException;
+import net.sf.saxon.tree.wrapper.VirtualNode;
+import net.sf.saxon.value.AnyURIValue;
+import net.sf.saxon.value.AtomicValue;
+import net.sf.saxon.value.Base64BinaryValue;
+import net.sf.saxon.value.BigDecimalValue;
+import net.sf.saxon.value.BooleanValue;
+import net.sf.saxon.value.DateTimeValue;
+import net.sf.saxon.value.DateValue;
+import net.sf.saxon.value.DayTimeDurationValue;
+import net.sf.saxon.value.DecimalValue;
+import net.sf.saxon.value.DoubleValue;
+import net.sf.saxon.value.DurationValue;
+import net.sf.saxon.value.ExternalObject;
+import net.sf.saxon.value.FloatValue;
+import net.sf.saxon.value.GDayValue;
+import net.sf.saxon.value.GMonthDayValue;
+import net.sf.saxon.value.GMonthValue;
+import net.sf.saxon.value.GYearMonthValue;
+import net.sf.saxon.value.GYearValue;
+import net.sf.saxon.value.HexBinaryValue;
+import net.sf.saxon.value.IntegerValue;
+import net.sf.saxon.value.NumericValue;
+import net.sf.saxon.value.QNameValue;
 import net.sf.saxon.value.SequenceType;
+import net.sf.saxon.value.StringValue;
+import net.sf.saxon.value.TimeValue;
+import net.sf.saxon.value.YearMonthDurationValue;
+import nl.armatiek.xslweb.configuration.Context;
+import nl.armatiek.xslweb.configuration.WebApp;
 import nl.armatiek.xslweb.saxon.functions.ExtensionFunctionCall;
 import nl.armatiek.xslweb.saxon.functions.ExtensionFunctionDefinition;
 
 public class DynamicExtensionFunctionDefinition extends ExtensionFunctionDefinition {
+  
+  private static HashMap<Class, SequenceType> jpmap = new HashMap<>();
+
+  static {
+    jpmap.put(Date.class, SequenceType.OPTIONAL_DATE_TIME);
+    jpmap.put(Duration.class, SequenceType.OPTIONAL_DURATION);
+    jpmap.put(QName.class, SequenceType.OPTIONAL_QNAME);
+  }
 
   private StructuredQName funcName;
   private int minArguments;
@@ -59,19 +112,183 @@ public class DynamicExtensionFunctionDefinition extends ExtensionFunctionDefinit
   public SequenceType[] getArgumentTypes() {
     SequenceType[] types = new SequenceType[parameterTypes.length];
     for (int i=0; i<parameterTypes.length; i++) {
-      types[i] = PJConverter.getEquivalentSequenceType(parameterTypes[i]);
+      types[i] = getSequenceType(parameterTypes[i]);
     }
     return types;
   }
   
   @Override
   public SequenceType getResultType(SequenceType[] suppliedArgumentTypes) {
-    return PJConverter.getEquivalentSequenceType(returnType);
+    return getSequenceType(returnType);
   }
   
   @Override
   public boolean hasSideEffects() {
     return hasSideEffects;
+  }
+  
+  private Object convertToJava(Item item, Class javaClass) throws XPathException {
+    if (item instanceof NodeInfo) {
+      Object node = item;
+      while (node instanceof VirtualNode) {
+        // strip off any layers of wrapping
+        node = ((VirtualNode) node).getRealNode();
+      }
+      return node;
+    } else if (item instanceof Function) {
+      return item;
+    } else if (item instanceof ExternalObject) {
+      return ((ExternalObject) item).getObject();
+    } else {
+      AtomicValue value = (AtomicValue) item;
+      switch (value.getItemType().getPrimitiveType()) {
+      case StandardNames.XS_STRING:
+      case StandardNames.XS_UNTYPED_ATOMIC:
+        if (javaClass.equals(String.class)) {
+          return value.getStringValue();
+        } else if (javaClass.equals(CharSequence.class)) {
+          return (CharSequence) value.getStringValue();
+        } else if (javaClass.equals(StringValue.class)) {
+          return new StringValue(value.getStringValue());
+        }
+      case StandardNames.XS_ANY_URI:
+        AnyURIValue av = (AnyURIValue) value;
+        try {
+          if (javaClass.equals(URI.class)) {
+            return new URI(av.getStringValue());
+          } else if (javaClass.equals(URL.class)) {
+            return new URL(av.getStringValue());
+          } else if (javaClass.equals(AnyURIValue.class)) {
+            return av;
+          }
+        } catch (URISyntaxException e) {
+          throw new XPathException("Syntax error in URI \"" + av.getStringValue() + "\"", e);
+        } catch (MalformedURLException e) {
+          throw new XPathException("Malformed URL \"" + av.getStringValue() + "\"", e);
+        }  
+      case StandardNames.XS_DURATION:
+        if (javaClass.equals(DurationValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_DAY_TIME_DURATION:
+        if (javaClass.equals(DayTimeDurationValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_YEAR_MONTH_DURATION:
+        if (javaClass.equals(YearMonthDurationValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_G_YEAR:
+        if (javaClass.equals(GYearValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_G_YEAR_MONTH:
+        if (javaClass.equals(GYearMonthValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_G_MONTH_DAY:
+        if (javaClass.equals(GMonthDayValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_G_MONTH:
+        if (javaClass.equals(GMonthValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_G_DAY:
+        if (javaClass.equals(GDayValue.class)) {
+          return value;
+        }  
+      case StandardNames.XS_BOOLEAN:
+        if (javaClass.equals(Boolean.class) || javaClass.equals(boolean.class)) {
+          return ((BooleanValue) value).getBooleanValue() ? Boolean.TRUE : Boolean.FALSE;
+        } else if (javaClass.equals(BooleanValue.class)) {
+          return item;
+        }
+      case StandardNames.XS_DECIMAL:
+        BigDecimal bd = ((BigDecimalValue) value).getDecimalValue();
+        if (javaClass.equals(Integer.class) || javaClass.equals(int.class)) {
+          return bd.intValue();
+        } else if (javaClass.equals(Long.class) || javaClass.equals(long.class)) {
+          return bd.longValue();
+        } else if (javaClass.equals(Short.class) || javaClass.equals(short.class)) {
+          return bd.shortValue();
+        } else if (javaClass.equals(Double.class) || javaClass.equals(double.class)) {
+          return bd.doubleValue();
+        } else if (javaClass.equals(Float.class) || javaClass.equals(float.class)) {
+          return bd.floatValue();
+        } else if (javaClass.equals(BigDecimal.class)) {
+          return bd;
+        } else if (javaClass.equals(DecimalValue.class)) {
+          return (DecimalValue) value;
+        }
+      case StandardNames.XS_INTEGER:
+        NumericValue nv = ((NumericValue) value);
+        if (javaClass.equals(Integer.class) || javaClass.equals(int.class)) {
+          return (int) nv.longValue();
+        } else if (javaClass.equals(Long.class) || javaClass.equals(long.class)) {
+          return (long) nv.longValue();
+        } else if (javaClass.equals(Short.class) || javaClass.equals(short.class)) {
+          return (short) nv.longValue();
+        } else if (javaClass.equals(Byte.class) || javaClass.equals(byte.class)) {
+          return (byte) nv.longValue();
+        } else if (javaClass.equals(BigInteger.class)) {
+          return BigInteger.valueOf(nv.longValue());
+        } else if (javaClass.equals(IntegerValue.class)) {
+          return (IntegerValue) value;
+        }
+      case StandardNames.XS_DOUBLE:
+        if (javaClass.equals(Double.class) || javaClass.equals(double.class)) {
+          return ((DoubleValue) value).getDoubleValue();
+        } else if (javaClass.equals(DoubleValue.class)) {
+          return (DoubleValue) value;
+        }
+      case StandardNames.XS_FLOAT:
+        if (javaClass.equals(Float.class) || javaClass.equals(float.class)) {
+          return ((FloatValue) value).getFloatValue();
+        } else if (javaClass.equals(FloatValue.class)) {
+          return (FloatValue) value;
+        }
+      case StandardNames.XS_DATE_TIME:
+        if (javaClass.equals(Date.class)) {
+          return ((DateTimeValue) value).getCalendar().getTime();
+        } else if (javaClass.equals(DateTimeValue.class)) {
+          return (DateTimeValue) value;
+        }
+      case StandardNames.XS_DATE:
+        if (javaClass.equals(Date.class)) {
+          return ((DateValue) value).getCalendar().getTime();
+        } else if (javaClass.equals(DateValue.class)) {
+          return (DateValue) value;
+        }
+      case StandardNames.XS_TIME:
+        if (javaClass.equals(TimeValue.class)) {
+          return (TimeValue) value;
+        }
+        return value.getStringValue();
+      case StandardNames.XS_BASE64_BINARY:
+        if (javaClass.equals(byte[].class)) {
+          return ((Base64BinaryValue) value).getBinaryValue();
+        } else if (javaClass.equals(Base64BinaryValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_HEX_BINARY:
+        if (javaClass.equals(byte[].class)) {
+          return ((HexBinaryValue) value).getBinaryValue();
+        } else if (javaClass.equals(HexBinaryValue.class)) {
+          return value;
+        }
+      case StandardNames.XS_QNAME:
+        if (javaClass.equals(QName.class)) {
+          return new QName(((QNameValue) value).getStructuredQName());
+        } else if (javaClass.equals(javax.xml.namespace.QName.class)) {
+          return ((QNameValue) value).toJaxpQName();
+        } else if (javaClass.equals(QNameValue.class)) {
+          return value;
+        }
+      default:
+        throw new XPathException("Conversion from \"" + value.getItemType().getDisplayName() + "\" to \"" + javaClass.toString() + "\" is not supported");
+      }
+    }
   }
   
   @Override
@@ -82,28 +299,156 @@ public class DynamicExtensionFunctionDefinition extends ExtensionFunctionDefinit
       public Sequence<?> call(XPathContext context, Sequence[] arguments) throws XPathException {
         try { 
           Method method = callClass.getMethod("call", parameterTypes);
-          ArrayList<Object> parameters = new ArrayList<Object>();
+          
+          ArrayList<Object> implicitObjects = new ArrayList<Object>();
+          for (Class paramType: parameterTypes) {
+            if (paramType.equals(XPathContext.class)) {
+              implicitObjects.add(context);
+              continue;
+            } else if (paramType.equals(Context.class)) {
+              implicitObjects.add(Context.getInstance());
+              continue;
+            } else if (paramType.equals(WebApp.class)) {
+              implicitObjects.add(getWebApp(context));
+              continue;
+            } else if (paramType.equals(HttpSession.class)) {
+              implicitObjects.add(getSession(context));
+              continue;
+            } else if (paramType.equals(HttpServletRequest.class)) {
+              implicitObjects.add(getRequest(context));
+              continue;
+            } else if (paramType.equals(HttpServletResponse.class)) {
+              implicitObjects.add(getResponse(context));
+              continue;
+            } 
+            break;
+          }
+          
+          Object[] parameters = new Object[implicitObjects.size() + arguments.length];
+          
+          if (parameters.length != parameterTypes.length) {
+            throw new XPathException(
+                String.format(
+                  "The number of supplied arguments in the call to the XPath extension function \"%s\" (%d) "
+                  + "does not match the number of declared arguments in the Java \"call\" method (%d)%s", 
+                  funcName.getClarkName(), 
+                  arguments.length,
+                  parameterTypes.length,
+                  implicitObjects.isEmpty() ? "" : ", considering \"" + implicitObjects.size() + " implicit objects"
+                ), 
+                "TODO");
+          }
+          
+          if (!implicitObjects.isEmpty()) {
+            System.arraycopy(implicitObjects.toArray(new Object[implicitObjects.size()]), 0, parameters, 0, implicitObjects.size());
+          }
+          
           for (int i=0; i<arguments.length; i++) {
             Sequence<Item<?>> argument = arguments[i];
-            Class paramType = parameterTypes[i];
+            Class paramType = parameterTypes[i + implicitObjects.size()];
             boolean isArray = paramType.isArray();
             Object param;
             if (isArray) {
+              Class componentType = paramType.getComponentType();
               ArrayList<Object> list = new ArrayList<Object>();
               SequenceIterator<Item<?>> si = argument.iterate();
               Item<?> item;
               while ((item = si.next()) != null) {
-                list.add(SequenceTool.convertToJava(item));
+                list.add(convertToJava(item, componentType));
               }
-              Object[] objArr = list.toArray(new Object[list.size()]);
-              param = Arrays.copyOf(objArr, objArr.length, paramType); 
+              if (componentType.isPrimitive()) {
+                if (componentType.equals(byte.class)) {
+                  byte[] byteArray = new byte[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    byteArray[j] = (byte) list.get(j);
+                  }
+                  param = byteArray;
+                } else if (componentType.equals(short.class)) {
+                  short[] shortArray = new short[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    shortArray[j] = (short) list.get(j);
+                  }
+                  param = shortArray;
+                } else if (componentType.equals(int.class)) {
+                  int[] intArray = new int[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    intArray[j] = (int) list.get(j);
+                  }
+                  param = intArray;
+                } else if (componentType.equals(long.class)) {
+                  long[] longArray = new long[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    longArray[j] = (long) list.get(j);
+                  }
+                  param = longArray;
+                } else if (componentType.equals(float.class)) {
+                  float[] floatArray = new float[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    floatArray[j] = (float) list.get(j);
+                  }
+                  param = floatArray;
+                } else if (componentType.equals(double.class)) {
+                  double[] doubleArray = new double[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    doubleArray[j] = (double) list.get(j);
+                  }
+                  param = doubleArray;
+                } else if (componentType.equals(boolean.class)) {
+                  boolean[] booleanArray = new boolean[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    booleanArray[j] = (boolean) list.get(j);
+                  }
+                  param = booleanArray;
+                } else if (componentType.equals(char.class)) {
+                  char[] charArray = new char[list.size()];
+                  for (int j=0; j<list.size(); j++) {
+                    charArray[j] = (char) list.get(j);
+                  }
+                  param = charArray;
+                } else {
+                  throw new UnsupportedOperationException("Unsupported primitive type");
+                }
+              } else {
+                Object[] objArr = list.toArray(new Object[list.size()]);
+                param = Arrays.copyOf(objArr, objArr.length, paramType);
+              }
             } else {
-              param = SequenceTool.convertToJava(argument.head());
+              /*
+              Object obj = convertToJava(argument.head(), paramType);
+              if (paramType.isPrimitive()) {
+                if (paramType.equals(byte.class)) {
+                  param = ((Byte) obj).byteValue();
+                } else if (paramType.equals(short.class)) {
+                  param = ((Short) obj).shortValue();
+                } else if (paramType.equals(int.class)) {
+                  if (obj.getClass().equals(Long.class)) {
+                    param = ((Long) obj).intValue();
+                  } else {
+                    param = ((Integer) obj).intValue();  
+                  }
+                } else if (paramType.equals(long.class)) {
+                  param = ((Long) obj).longValue();
+                } else if (paramType.equals(float.class)) {
+                  param = ((Float) obj).floatValue();
+                } else if (paramType.equals(double.class)) {
+                  param = ((Double) obj).doubleValue();
+                } else if (paramType.equals(boolean.class)) {
+                  param = ((Boolean) obj).booleanValue();
+                } else if (paramType.equals(char.class)) {
+                  param = ((Character) obj).charValue();
+                } else {
+                  throw new UnsupportedOperationException("Unsupported primitive type");
+                }
+              } else {
+                param = obj;
+              }
+              */
+              param = convertToJava(argument.head(), paramType);
             }
-            parameters.add(param);
+            parameters[i + implicitObjects.size()] = param;
           }
           Object callObj = callClass.newInstance();
-          Object resultObj = method.invoke(callObj, parameters.toArray());
+          Object resultObj = method.invoke(callObj, parameters);
           JPConverter conv = JPConverter.allocate(returnType, null, context.getConfiguration());
           return conv.convert(resultObj, context);
         } catch (NoSuchMethodException e) {
@@ -119,6 +464,26 @@ public class DynamicExtensionFunctionDefinition extends ExtensionFunctionDefinit
         }
       }
     };
+    
+  }
+  
+  private SequenceType getOverrideSequenceType(Class javaClass) {
+    if (javaClass.isArray()) {
+      Class memberClass = javaClass.getComponentType();
+      SequenceType memberType = getOverrideSequenceType(memberClass);
+      if (memberType != null) {
+        return SequenceType.makeSequenceType(memberType.getPrimaryType(), StaticProperty.ALLOWS_ZERO_OR_MORE);
+      }
+    }
+    return jpmap.get(javaClass);
+  }
+  
+  private SequenceType getSequenceType(Class javaClass) {
+    SequenceType type = getOverrideSequenceType(javaClass);
+    if (type == null) {
+      type = PJConverter.getEquivalentSequenceType(javaClass);
+    }
+    return type;
   }
   
 }
