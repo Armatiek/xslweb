1cac3019facad5119c209782ceeae2ee035b5142
Maarten Kroon <maarten.kroon@koop.overheid.nl>
2020-09-04 19:26:36 +0200

    Increased version number to 4.0.0-RC1

--- src/main/java/nl/armatiek/xslweb/configuration/Definitions.java
+++ src/main/java/nl/armatiek/xslweb/configuration/Definitions.java
@@ -33,7 +33,7 @@ import net.sf.saxon.s9api.QName;
 public class Definitions {
   
   public final static String PROJECT_NAME                      = "xslweb";
-  public final static String PROJECT_VERSION                   = "3.3.0";
+  public final static String PROJECT_VERSION                   = "4.0.0-RC1";
   public final static String PATHNAME_REQUESTDISPATCHER_XSL    = "xsl/request-dispatcher.xsl";
   public final static String FILENAME_PROPERTIES               = "xslweb.properties";
   public final static String FILENAME_QUARTZ                   = "xslweb-quartz.properties";
