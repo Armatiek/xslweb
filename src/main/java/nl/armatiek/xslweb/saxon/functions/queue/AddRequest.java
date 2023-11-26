8dcc6570e5fc8eb60784538ad4a870aea86fe98e
maartenkroon <maarten.kroon@armatiek.nl>
2023-11-26 18:05:09 +0100

    Pass lightweight clone of HttpServletRequest to ExecutorService instead of request itself because of exception "java.lang.IllegalStateException: The request object has been recycled and is no longer associated with this facade" under Tomcat 10 (and maybe other application servers).

--- src/main/java/nl/armatiek/xslweb/saxon/functions/queue/AddRequest.java
+++ src/main/java/nl/armatiek/xslweb/saxon/functions/queue/AddRequest.java
@@ -1,18 +1,40 @@
 package nl.armatiek.xslweb.saxon.functions.queue;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -82,6 +104,9 @@ public class AddRequest extends ExtensionFunctionDefinition {
         String path = ((StringValue) arguments[1].head()).getStringValue();
         WebApp webApp = getWebApp(context);
         HttpServletRequest request = getRequest(context);
+        if (!StringUtils.equalsIgnoreCase("GET", request.getMethod())) {
+          throw new XPathException("A queued request must have method \"GET\"");
+        }
         String extraInfo = arguments.length > 2 ? serialize((NodeInfo) arguments[2].head(), webApp.getProcessor()) : null;
         ExecutorService service = webApp.getExecutorService(queueName);
         String ticket = UUID.randomUUID().toString();
@@ -91,7 +116,7 @@ public class AddRequest extends ExtensionFunctionDefinition {
         File lockFile = new File(queueDir, ticket + ".lck");
         FileUtils.touch(lockFile);
         try {
-          service.execute(new QueuedRequest(ticket, webApp.getPath() + "/" + path, extraInfo, request));
+          service.execute(new QueuedRequest(ticket, webApp.getPath() + "/" + path, extraInfo, new QueuedHttpServletRequestClone(request)));
         } catch (RejectedExecutionException ree) {
           FileUtils.deleteQuietly(lockFile);
           return StringValue.makeStringValue("rejected");
@@ -147,10 +172,371 @@ public class AddRequest extends ExtensionFunctionDefinition {
           FileUtils.deleteQuietly(lockFile);
         }
       } catch (Exception e) {
-        logger.error("Error executing asynchronous request \"" + path + "\"", e);
+        logger.error("Error executing queued/asynchronous request \"" + path + "\"", e);
       }
     }
    
+  }
+  
+  /**
+   * Lightweight GET only clone of a HttpServletRequest that copies only the essential data
+   * necessary for a queued/asynchronous request. 
+   */
+  private static class QueuedHttpServletRequestClone implements HttpServletRequest {
+      
+    private final HashMap<String, Object> attributes = new HashMap<>();
+    private final HashMap<String, Enumeration<String>> headers = new HashMap<>();
+    private final HashMap<String, String[]> parameters = new HashMap<>();
+    private final String authType;
+    private final String characterEncoding;
+    private final int contentLength;
+    private final String contentType;
+    private final String method;
+    private final String pathInfo;
+    private final String protocol;
+    private final String pathTranslated;
+    private final String scheme;
+    private final String serverName;
+    private final String contextPath;
+    private final int serverPort; 
+    private final String queryString;
+    private final String remoteAddr;
+    private final String remoteUser;
+    private final String remoteHost;
+    private final String requestedSessionId;
+    private final String requestURI;
+    private final StringBuffer requestURL;
+    private final boolean isSecure;
+    private final String servletPath;
+    private final int remotePort;
+    private final String localName;
+    private final String localAddr;
+    private final int localPort;
+    private final boolean isRequestedSessionIdValid;
+    private final boolean isRequestedSessionIdFromCookie;
+    private final boolean isRequestedSessionIdFromURL;
+    private final boolean isRequestedSessionIdFromUrl;
+    
+    @SuppressWarnings("deprecation")
+    public QueuedHttpServletRequestClone(HttpServletRequest request) {
+      Enumeration<String> attrs = request.getAttributeNames();
+      while (attrs.hasMoreElements()) {
+        String name = attrs.nextElement();
+        attributes.put(name, request.getAttribute(name));
+      }
+      Enumeration<String> hdrs = request.getHeaderNames();
+      while (hdrs.hasMoreElements()) {
+        String name = hdrs.nextElement();
+        headers.put(name, request.getHeaders(name));
+      }
+      Enumeration<String> params = request.getParameterNames();
+      while (params.hasMoreElements()) {
+        String name = params.nextElement();
+        parameters.put(name, request.getParameterValues(name));
+      }
+      authType = request.getAuthType();
+      characterEncoding = request.getCharacterEncoding();
+      contentLength = request.getContentLength();
+      contentType = request.getContentType();
+      method = request.getMethod();
+      pathInfo = request.getPathInfo();
+      protocol = request.getProtocol();
+      pathTranslated = request.getPathTranslated();
+      scheme = request.getScheme();
+      serverName = request.getServerName();
+      contextPath = request.getContextPath();
+      serverPort = request.getServerPort();
+      queryString = request.getQueryString();
+      remoteAddr = request.getRemoteAddr();
+      remoteUser = request.getRemoteUser();
+      remoteHost = request.getRemoteHost();
+      requestedSessionId = request.getRequestedSessionId();
+      requestURI = request.getRequestURI();
+      requestURL = new StringBuffer(request.getRequestURL());
+      isSecure = request.isSecure();
+      servletPath = request.getServletPath();
+      remotePort = request.getRemotePort();
+      localName = request.getLocalName();
+      localAddr = request.getLocalAddr();
+      localPort = request.getLocalPort();
+      isRequestedSessionIdValid = request.isRequestedSessionIdValid();
+      isRequestedSessionIdFromCookie = request.isRequestedSessionIdFromCookie();
+      isRequestedSessionIdFromURL = request.isRequestedSessionIdFromURL();
+      isRequestedSessionIdFromUrl = request.isRequestedSessionIdFromUrl();
+    }
+
+    public Object getAttribute(String name) {
+      return attributes.get(name);
+    }
+
+    public String getAuthType() {
+      return authType;
+    }
+
+    public Cookie[] getCookies() {
+      throw new NotImplementedException();
+    }
+
+    public Enumeration<String> getAttributeNames() {
+      return Collections.enumeration(attributes.keySet());
+    }
+
+    public long getDateHeader(String name) {
+      throw new NotImplementedException();
+    }
+
+    public String getCharacterEncoding() {
+      return characterEncoding;
+    }
+
+    public void setCharacterEncoding(String encoding) throws UnsupportedEncodingException {
+      throw new NotImplementedException();
+    }
+
+    public String getHeader(String name) {
+      Enumeration<String> values = headers.get(name);
+      if (values.hasMoreElements()) {
+        return values.nextElement();
+      }
+      return null;
+    }
+
+    public int getContentLength() {
+      return contentLength;
+    }
+
+    public String getContentType() {
+      return contentType;
+    }
+
+    public Enumeration<String> getHeaders(String name) {
+      return headers.get(name);
+    }
+
+    public ServletInputStream getInputStream() throws IOException {
+      throw new NotImplementedException();
+    }
+
+    public String getParameter(String name) {
+      String[] params = parameters.get(name);
+      if (params != null && params.length > 0) {
+        return params[0];
+      }
+      return null;
+    }
+
+    public Enumeration<String> getHeaderNames() {
+      return Collections.enumeration(headers.keySet());
+    }
+
+    public int getIntHeader(String name) {
+      throw new NotImplementedException();
+    }
+
+    public Enumeration<String> getParameterNames() {
+      return Collections.enumeration(parameters.keySet());
+    }
+
+    public String[] getParameterValues(String name) {
+      return parameters.get(name);
+    }
+
+    public String getMethod() {
+      return method;
+    }
+
+    public String getPathInfo() {
+      return pathInfo;
+    }
+
+    public Map<String, String[]> getParameterMap() {
+      return parameters;
+    }
+
+    public String getProtocol() {
+      return protocol;
+    }
+
+    public String getPathTranslated() {
+      return pathTranslated;
+    }
+
+    public String getScheme() {
+      return scheme;
+    }
+
+    public String getServerName() {
+      return serverName;
+    }
+
+    public String getContextPath() {
+      return contextPath;
+    }
+
+    public int getServerPort() {
+      return serverPort;
+    }
+
+    public BufferedReader getReader() throws IOException {
+      throw new NotImplementedException();
+    }
+
+    public String getQueryString() {
+      return queryString;
+    }
+
+    public String getRemoteAddr() {
+      return remoteAddr;
+    }
+
+    public String getRemoteUser() {
+      return this.remoteUser;
+    }
+
+    public String getRemoteHost() {
+      return remoteHost;
+    }
+
+    public boolean isUserInRole(String role) {
+      throw new NotImplementedException();
+    }
+
+    public void setAttribute(String name, Object o) {
+      attributes.put(name, o);
+    }
+
+    public Principal getUserPrincipal() {
+      throw new NotImplementedException();
+    }
+
+    public String getRequestedSessionId() {
+      return requestedSessionId;
+    }
+
+    public void removeAttribute(String name) {
+      attributes.remove(name);
+    }
+
+    public String getRequestURI() {
+      return requestURI;
+    }
+
+    public Locale getLocale() {
+      throw new NotImplementedException();
+    }
+
+    public Enumeration<Locale> getLocales() {
+      throw new NotImplementedException();
+    }
+
+    public StringBuffer getRequestURL() {
+      return requestURL;
+    }
+
+    public boolean isSecure() {
+      return isSecure;
+    }
+
+    public RequestDispatcher getRequestDispatcher(String path) {
+      throw new NotImplementedException();
+    }
+
+    public String getServletPath() {
+      return servletPath;
+    }
+
+    public HttpSession getSession(boolean create) {
+      return null;
+    }
+
+    public String getRealPath(String path) {
+      throw new NotImplementedException();
+    }
+
+    public int getRemotePort() {
+      return remotePort;
+    }
+
+    public String getLocalName() {
+      return localName;
+    }
+
+    public String getLocalAddr() {
+      return localAddr;
+    }
+
+    public HttpSession getSession() {
+      return null;
+    }
+
+    public int getLocalPort() {
+      return localPort;
+    }
+
+    public ServletContext getServletContext() {
+      throw new NotImplementedException();
+    }
+
+    public boolean isRequestedSessionIdValid() {
+      return isRequestedSessionIdValid;
+    }
+
+    public AsyncContext startAsync() throws IllegalStateException {
+      throw new NotImplementedException();
+    }
+
+    public boolean isRequestedSessionIdFromCookie() {
+      return isRequestedSessionIdFromCookie;
+    }
+
+    public boolean isRequestedSessionIdFromURL() {
+      return isRequestedSessionIdFromURL;
+    }
+
+    public boolean isRequestedSessionIdFromUrl() {
+      return isRequestedSessionIdFromUrl;
+    }
+
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+      throw new NotImplementedException();
+    }
+
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+      throw new NotImplementedException();
+    }
+
+    public void login(String username, String password) throws ServletException {
+      throw new NotImplementedException();
+    }
+
+    public void logout() throws ServletException {
+      throw new NotImplementedException();
+    }
+
+    public Collection<Part> getParts() throws IOException, ServletException {
+      throw new NotImplementedException();
+    }
+
+    public boolean isAsyncStarted() {
+      throw new NotImplementedException();
+    }
+
+    public Part getPart(String name) throws IOException, ServletException {
+      throw new NotImplementedException();
+    }
+
+    public boolean isAsyncSupported() {
+      return false;
+    }
+
+    public AsyncContext getAsyncContext() {
+      throw new NotImplementedException();
+    }
+
+    public DispatcherType getDispatcherType() {
+      throw new NotImplementedException();
+    }
+    
   }
   
 }
